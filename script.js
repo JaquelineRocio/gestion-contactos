b15d0151e3c16c0f65e7e4af1e1f4dcd86a1b935
JaquelineRocio <jaquelineramosvargas@gmail.com>
2024-09-17 22:11:22 -0500

    feat: create, delete and update contacts

---
+++ script.js
@@ -0,0 +1,61 @@
+const boton = document.querySelector("#addContact");
+const contactos = [];
+const contactList = document.querySelector("#contactList");
+boton.addEventListener("click", () => {
+  let inputValue = document.querySelector("#contactName").value;
+  const existeContacto = contactos.indexOf(inputValue);
+  if (inputValue != "" && existeContacto) {
+    contactos.push(inputValue);
+
+    const li = document.createElement("li");
+
+    const inputContactName = document.createElement("input");
+    inputContactName.value = inputValue;
+    inputContactName.disabled = true;
+    li.appendChild(inputContactName);
+
+    const buttonDelete = document.createElement("button");
+    buttonDelete.innerText = "Eliminar";
+    buttonDelete.className = "button-delete";
+    buttonDelete.addEventListener("click", eliminarElemento);
+    li.appendChild(buttonDelete);
+
+    const buttonEdit = document.createElement("button");
+    buttonEdit.innerText = "Actualizar";
+    buttonEdit.className = "button-edit";
+    buttonEdit.addEventListener("click", actualizarElemento);
+    li.appendChild(buttonEdit);
+    contactList.appendChild(li);
+  }
+});
+
+function eliminarElemento(event) {
+  const boton = event.target;
+  const li = boton.parentElement;
+  const index = Array.from(contactList.children).indexOf(li);
+
+  if (index > -1) {
+    contactos.splice(index, 1);
+  }
+
+  contactList.removeChild(li);
+  console.log(contactos);
+}
+
+function actualizarElemento(event) {
+  const boton = event.target;
+  const li = boton.parentElement;
+  const inputContactName = li.querySelector("input");
+  const buttonEdit = li.querySelector(".button-edit");
+
+  if (buttonEdit.innerText === "Actualizar") {
+    inputContactName.disabled = false;
+    buttonEdit.innerText = "Guardar";
+  } else {
+    const index = Array.from(contactList.children).indexOf(li);
+    contactos[index] = inputContactName.value;
+    inputContactName.disabled = true;
+    buttonEdit.innerText = "Actualizar";
+    console.log(contactos);
+  }
+}
